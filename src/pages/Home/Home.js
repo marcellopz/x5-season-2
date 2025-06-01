6a6813343b2541167a1421adf6113ddf4a1ae8f4
marcellopz <marcellopz@hotmail.com>
2025-06-01 13:40:39 -0300

    homepage changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
 import { getOverallStats, getPlayer } from "../../services/firebaseDatabase";
 import { MiscContext } from "../../contexts/miscContext";
-import CardDisplay from "../../common-components/CardDisplay/CardDisplay";
 import X5pageContentArea from "../../common-components/X5pageContentArea";
-import { Box, Typography } from "@mui/material";
-import OverallStats from "../GameStats/OverallStats";
+import MainSection from "./Sections/MainSection";
+import PlaytimeSection from "./Sections/PlaytimeSection";
+import OverallStatsSection from "./Sections/OverallStatsSection";
 import "./home.css";
-import GeneralStatsBox from "./GeneralStatsBox";
-import GamesGraph from "./GamesGraph";
-import { AuthContext } from "../../contexts/authContext";
 
 export default function Home() {
-  const { isAnonymous } = useContext(AuthContext);
   const { getCardbackground } = useContext(MiscContext);
   const [players, setPlayers] = useState(null);
   const [stats, setStats] = useState({});
@@ -36,35 +32,11 @@ export default function Home() {
     getCardbackground();
   }, [getCardbackground]);
 
-  if (!loading && (stats === null || players === null)) {
-    return (
-      <div style={{ display: "flex", marginTop: "100px" }}>No data yet</div>
-    );
-  }
-
   return (
     <X5pageContentArea noBackground removeMarginTop loading={loading}>
-      <Box className="grid-container">
-        <Box
-          className="grid-item big-item"
-          sx={{
-            minHeight: "470px",
-          }}
-        >
-          <Typography variant="h5" marginBottom={1} fontWeight={500}>
-            Legend Cards
-          </Typography>
-          <CardDisplay players={players} />
-          {isAnonymous && (
-            <span>
-              To see the card images, please log in and request permission.
-            </span>
-          )}
-        </Box>
-        <GeneralStatsBox stats={stats} players={players} />
-        <GamesGraph stats={stats} />
-      </Box>
-      <OverallStats stats={stats} hideMainStats />
+      <MainSection stats={stats} players={players} />
+      <PlaytimeSection stats={stats} />
+      <OverallStatsSection stats={stats} />
     </X5pageContentArea>
   );
 }
